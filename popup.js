945bbf61db0b3c6080ef209aecdc949af054b7cd
Eric Xue <eric.xy.xue@gmail.com>
2021-05-04 16:38:17 -0700

    Create 0.1.0-alpha02

--- popup.js
+++ popup.js
@@ -7,5 +7,20 @@ const value2 = document.getElementById("value2");
 const convert = document.getElementById("convert");
 
 convert.onclick = (_) => {
-	value2.innerHTML = parseInt(value1.value, parseInt(base1.value)).toString(parseInt(base2.value));
+	const number = parseInt(value1.value, parseInt(base1.value)).toString(parseInt(base2.value));
+	if (base2.value === "2" && number !== NaN.toString()) {
+		const bits = number.split("").join("|");
+		const dashes = [];
+		for (let index = 0; index < bits.length; index++) {
+			let element = bits[index];
+			dashes.push(element === "|" ? "+" : "-");
+		}
+		const places = [];
+		for (let index = 0; index < number.length; index++) {
+			places.unshift(index + 1);
+		}
+		value2.innerHTML = `${bits}<br>${dashes.join("")}<br>${places.map(value => value.toString()).join("|")}`;
+	} else {
+		value2.innerHTML = number;
+	}	
 };
