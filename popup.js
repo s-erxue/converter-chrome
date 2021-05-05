bb9c9d113c8d0c264cb30d8b5ccd821cade1eb29
Eric Xue <eric.xy.xue@gmail.com>
2021-05-05 12:04:45 -0700

    Create 0.1.0-alpha03

--- popup.js
+++ popup.js
@@ -9,17 +9,15 @@ const convert = document.getElementById("convert");
 convert.onclick = (_) => {
 	const number = parseInt(value1.value, parseInt(base1.value)).toString(parseInt(base2.value));
 	if (base2.value === "2" && number !== NaN.toString()) {
-		const bits = number.split("").join("|");
-		const dashes = [];
-		for (let index = 0; index < bits.length; index++) {
-			let element = bits[index];
-			dashes.push(element === "|" ? "+" : "-");
-		}
-		const places = [];
-		for (let index = 0; index < number.length; index++) {
-			places.unshift(index + 1);
-		}
-		value2.innerHTML = `${bits}<br>${dashes.join("")}<br>${places.map(value => value.toString()).join("|")}`;
+		value2.innerHTML = "";
+		const table = document.createElement("table");
+		const bits = table.insertRow();
+		const places = table.insertRow();
+		Array.from(number).forEach((value, index) => {
+			bits.insertCell().innerText = value.toString();
+			places.insertCell().innerText = number.length - index
+		})
+		value2.appendChild(table);
 	} else {
 		value2.innerHTML = number;
 	}	
